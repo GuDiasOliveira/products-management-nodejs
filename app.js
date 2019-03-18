00b39708a660541ead2ee38639ad9be2ba30fd82
Gustavo Dias de Oliveira <gust.dias@gmail.com>
2019-03-18 14:23:25 -0300

    Setup database and its entities. Setup env vars.

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+require('dotenv').config();
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const helmet = require('helmet');
+const database = require('./database');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -16,7 +18,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+database().then(db => {
+  app.use('/', indexRouter);
+  app.use('/users', usersRouter);
+  console.log('API routes ready!');
+}).catch(err => {
+  console.error('Failed to setup database');
+  console.error(err);
+  process.exit(1);
+});
 
 module.exports = app;
---
+++ database/index.js
@@ -0,0 +1,56 @@
+const Sequelize = require('sequelize');
+
+const host = process.env.DB_HOST;
+const user = process.env.DB_USER;
+const password = process.env.DB_PASSWORD;
+const database = process.env.DB_NAME;
+
+const sequelize = new Sequelize(database, user, password, {
+  host,
+  dialect: 'mysql',
+  logging: false,
+});
+
+const Category = sequelize.define('category', {
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+});
+
+const Product = sequelize.define('product', {
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: Sequelize.TEXT,
+    allowNull: false,
+  },
+  value: {
+    type: Sequelize.DECIMAL(12,2),
+    allowNull: false,
+  },
+});
+
+Product.belongsTo(Category);
+
+module.exports = async function() {
+  await sequelize.sync();
+  return {
+    sequelize,
+    models: {
+      Category, Product,
+    },
+  };
+}
